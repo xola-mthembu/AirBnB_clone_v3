5fb74deb4ec7726f05e5d5f895bb6a55a6ce9867
Sihle <sihle.mtshali8@gmail.com>
2024-05-27 15:54:37 -0700

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,3 +14,11 @@ def teardown_db(exception):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, threaded=True)
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, threaded=True)

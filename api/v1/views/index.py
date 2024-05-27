0aabb0ea68b5f79655fa02533c9dcf331a1da2dd
Sihle <sihle.mtshali8@gmail.com>
2024-05-27 15:15:16 -0700

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,21 @@
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Returns the number of each object by type"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })

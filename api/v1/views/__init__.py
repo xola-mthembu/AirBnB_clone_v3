852608cb48f49be674a94c443f83cb03c3d05af9
Sihle <sihle.mtshali8@gmail.com>
2024-05-27 17:06:02 -0700

    11. Reviews

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Initialize Blueprint for views"""
+"""
+Package initializer for app_views
+"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
@@ -10,3 +12,4 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
+from api.v1.views.places_reviews import *
